d5cb99bd9a41bc7955470e629ff407e68f63516b
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-30 22:05:42 +0200

    add production mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: [],
-  darkMode:'class', // or 'media' or 'class'
+  purge: ['public/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
